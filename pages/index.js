1427476ca81a4cd41f87844adebf980ac3b93152
Guilherme Rocha <guilherme.rocha@linktou.com.br>
2022-12-02 12:29:39 -0300

    Adjust home route

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,43 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { FileSearch, MagnifyingGlass } from "phosphor-react";
+import React, { useState } from "react";
 
-export default function Home() {
-  const router = useRouter();
+/** COMPONENTS */
+import Card from "../components/Card";
+import Content from "../components/Content";
+import { Header } from "../components/Header";
+import Input from "../components/Input";
+import List from "../components/List";
+/** END COMPONENTS */
 
-  useEffect(() => {
-    router.push({
-      pathname: "/home",
-    });
-  }, [router]);
+/** LAYOUT */
+import DefaultLayout from "../layout/DefaultLayout";
+/** END LAYOUT */
 
-  return null;
-}
+const Home = () => {
+	const [filter, setFilter] = useState("");
+
+	const handleOnChange = (e) => {
+		e.preventDefault();
+		setFilter(e.target.value);
+	};
+
+	return (
+		<DefaultLayout>
+			<Header
+				actions={
+					<Input
+						icon={<MagnifyingGlass size={18} />}
+						onChange={(e) => handleOnChange(e)}
+						placeholder={"Search a task"}
+					/>
+				}
+			/>
+			<Content>
+				<Card />
+				<List filter={filter} />
+			</Content>
+		</DefaultLayout>
+	);
+};
+
+export default Home;
